444daa5252ffe51418eb1e4582ba1aa719e05d6e
Laurent NOEL <laurent.noel.c2ba@gmail.com>
2019-10-23 15:23:59 +0200

    Clean react template, add Todos and TodoItem components

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem'
+
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  render = () => this.props.todos.map((todo) => (<TodoItem key={todo.id} todo={todo} />))
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
+
+export default Todos;
